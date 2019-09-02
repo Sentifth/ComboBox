aa12dedbd350cd6dff0316e94e22f075f2e2b734
Alumno <Alumno@PC30>
2019-09-02 10:12:54 -0500

    1.1

---
+++ ComboBox/ComboBox/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cbAlimentos = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(450, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(410, 216);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 219);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // cbAlimentos
+            // 
+            this.cbAlimentos.FormattingEnabled = true;
+            this.cbAlimentos.Location = new System.Drawing.Point(80, 44);
+            this.cbAlimentos.Name = "cbAlimentos";
+            this.cbAlimentos.Size = new System.Drawing.Size(445, 21);
+            this.cbAlimentos.TabIndex = 6;
+            this.cbAlimentos.SelectedIndexChanged += new System.EventHandler(this.CbAlimentos_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 389);
+            this.Controls.Add(this.cbAlimentos);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cbAlimentos;
+    }
+}
+
---
+++ ComboBox/ComboBox/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice=cbAlimentos.SelectedIndex;
+            string mensaje = "El día de hoy es " + cbAlimentos.Items[indice].ToString();
+            MessageBox.Show(mensaje);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbAlimentos.Items.Add("Pozole");
+            cbAlimentos.Items.Add("Tostadas de Pata");
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            cbAlimentos.Items.Add(textBox1.Text);
+        }
+
+        private void CbAlimentos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = cbAlimentos.SelectedIndex;
+            label1.Text = indice.ToString();
+            label2.Text = cbAlimentos.Items[indice].ToString();
+        }
+    }
+}
